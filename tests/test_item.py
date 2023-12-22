222b00163d042100311ac5e7556cb3788c26cf40
Tomek <klingaroo@gmail.com>
2023-12-22 11:44:34 -0500

    adds ItemRL callno tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,7 +7,7 @@ from pydantic import ValidationError
 from src.models import ItemRL
 
 
-def test_item_callno_tag_correct():
+def test_item_callno_tag_valid():
     with does_not_raise():
         ItemRL(callno_tag="8528", callno="ReCAP 23-108092")
 
@@ -16,3 +16,28 @@ def test_item_callno_tag_correct():
 def test_item_callno_tag_invalid(arg):
     with pytest.raises(ValidationError):
         ItemRL(callno_tag=arg, callno="ReCAP 23-108092")
+
+
+@pytest.mark.parametrize("arg", ["ReCAP 23-108092", "ReCAP 23-108093"])
+def test_item_callno_valid(arg):
+    with does_not_raise():
+        ItemRL(callno_tag="8528", callno=arg)
+
+
+@pytest.mark.parametrize(
+    "arg",
+    [
+        "ReCAP23-108092",
+        "Rcap 23-108093",
+        "ReCAP 23108093",
+        "ReCAP 22-108092",
+        "ReCAP 23-10809",
+        "ReCAP 23-108092 ",
+        " ReCAP 23-108092",
+        "ReCAP  23-108092" "",
+        None,
+    ],
+)
+def test_item_callno_valid(arg):
+    with pytest.raises(ValidationError):
+        ItemRL(callno_tag="8528", callno=arg)
