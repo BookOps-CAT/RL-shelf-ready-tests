8ce4fdf2cabb8ac582f9b130dc63d0facdad35a7
Tomek <klingaroo@gmail.com>
2023-12-26 08:26:05 -0500

    add error catching experiment

---
+++ run.py
@@ -0,0 +1,33 @@
+from pydantic import ValidationError
+from rich import print
+
+from src.models import OrderRL, ItemRL
+
+
+def parse_errors(e):
+    return f"{e['loc'][0]}: {e['msg']} | input: {e['input']}"
+
+
+def simulate():
+    try:
+        order = OrderRL(
+            location="myn",
+            fund_code="foo",
+            price="9.99",
+        )
+    except ValidationError as exc:
+        for e in exc.errors():
+            formatted_e = parse_errors(e)
+            print(f"[italic red]Order problem:[/italic red] {formatted_e}")
+    try:
+        item = ItemRL(
+            location="rc2ma", price="999", callno_tag="8528", callno="23-123456"
+        )
+    except ValidationError as exc:
+        for e in exc.errors():
+            formatted_e = parse_errors(e)
+            print(f"[italic red]Item problem:[/italic red] {formatted_e}")
+
+
+if __name__ == "__main__":
+    simulate()
